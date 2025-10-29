f9dfbcc5f902e8bfe6dde30c85916b59d4ab12a5
GilSorek <gil.sorek@cytoreason.com>
2025-10-29 07:38:15 +0000

    Top 1000 variable features PCA

--- notes/data_intake/s05_4hr_data_intake.R
+++ notes/data_intake/s05_4hr_data_intake.R
@@ -52,3 +52,24 @@ s05_4hr_sample_loadings_cells_PCA = statistic_table(cells_PCA)$sample_loadings %
 bq_job <- bq_perform_upload(bq_table("cytoreason","s05_atopic_dermatitis",table="s05_4hr_cells_PCA_sample_loadings"),
                   s05_4hr_sample_loadings_cells_PCA,write_disposition = "WRITE_TRUNCATE")
 bq_job_status(bq_job)
+
+# Top 1000 variable features PCA
+# Find top 1000 variable features
+expr_matrix = exprs(s05_4hr_raw_eset)
+gene_variability <- apply(expr_matrix, 1, var)
+top_genes <- names(sort(gene_variability, decreasing = TRUE))[1:1000]
+
+# Filter eset for top variable genes
+s05_4hr_bulk_eset_top1000_var_genes = s05_4hr_raw_eset[top_genes,]
+
+# Train the top 1000 features PCA
+features_top1000_PCA <- cytoreason.analytics::service_pca(object = s05_4hr_bulk_eset_top1000_var_genes, n_pc = 8)
+
+# Extract top 1000 features PCA sample loadings
+s05_4hr_sample_loadings_top1000_features_PCA = statistic_table(features_top1000_PCA)$sample_loadings %>% 
+  tidyr::pivot_wider(names_from = pc, values_from = value)
+  # dplyr::mutate(Sample=sample_id) %>% 
+  # left_join(pData(s05_4hr_raw_eset), by = "Sample")
+bq_job <- bq_perform_upload(bq_table("cytoreason","s05_atopic_dermatitis",table="s05_4hr_top1000_features_PCA_sample_loadings"),
+                  s05_4hr_sample_loadings_top1000_features_PCA,write_disposition = "WRITE_TRUNCATE")
+bq_job_status(bq_job)
